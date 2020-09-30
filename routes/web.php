0e1ff9f139d4556825fe1c1b88a53650913c34bf
shantudash <shantudasbd@yahoo.com>
2020-09-30 14:52:31 +0600

    namspace update in routes

--- routes/web.php
+++ routes/web.php
@@ -15,30 +15,47 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+//User routes starts here
 //Auth::routes();
+Route::namespace('Auth\User')->group(function () {
+    Route::get('login', 'LoginController@showLoginForm')->name('login');
+    Route::post('login', 'LoginController@login');
+    Route::post('logout', 'LoginController@logout')->name('logout');
+    Route::get('register', 'RegisterController@showRegistrationForm')->name('register');
+    Route::post('register', 'RegisterController@register');
+});
+Route::namespace('User')->group(function () {
+    Route::get('dashboard', 'DashboardController@index')->name('dashboard');
+});
+//User routes starts here
 
-Route::get('login', 'Auth\User\LoginController@showLoginForm')->name('login');
-Route::post('login', 'Auth\User\LoginController@login');
-Route::post('logout', 'Auth\User\LoginController@logout')->name('logout');
-Route::get('register', 'Auth\User\RegisterController@showRegistrationForm')->name('register');
-Route::post('register', 'Auth\user\RegisterController@register');
-Route::get('dashboard', 'User\DashboardController@index')->name('dashboard');
-
-
-Route::prefix('author')->group(function () {
-    Route::get('login', 'Auth\Author\LoginController@showLoginForm')->name('author.login');
-    Route::post('login', 'Auth\Author\LoginController@login');
-    Route::post('logout', 'Auth\Author\LoginController@logout')->name('author.logout');
-    Route::get('register', 'Auth\Author\RegisterController@showRegistrationForm')->name('author.register');
-    Route::post('register', 'Auth\Author\RegisterController@register');
-    Route::get('dashboard', 'Author\DashboardController@index')->name('author.dashboard');
+//Author routes starts here
+Route::prefix('author')->namespace('Auth\Author')->group(function () {
+    Route::get('login', 'LoginController@showLoginForm')->name('author.login');
+    Route::post('login', 'LoginController@login');
+    Route::post('logout', 'LoginController@logout')->name('author.logout');
+    Route::get('register', 'RegisterController@showRegistrationForm')->name('author.register');
+    Route::post('register', 'RegisterController@register');
 });
 
-Route::prefix('admin')->group(function () {
-    Route::get('login', 'Auth\Admin\LoginController@showLoginForm')->name('admin.login');
-    Route::post('login', 'Auth\Admin\LoginController@login');
-    Route::post('logout', 'Auth\Admin\LoginController@logout')->name('admin.logout');
-    Route::get('register', 'Auth\Admin\RegisterController@showRegistrationForm')->name('admin.register');
-    Route::post('register', 'Auth\Admin\RegisterController@register');
-    Route::get('dashboard', 'Admin\DashboardController@index')->name('admin.dashboard');
+Route::prefix('author')->namespace('Author')->group(function () {
+    Route::get('dashboard', 'DashboardController@index')->name('author.dashboard');
 });
+//Author routes ends here
+
+
+//Admin routes starts here
+
+Route::prefix('admin')->namespace('Auth\Admin')->group(function () {
+    Route::get('login', 'LoginController@showLoginForm')->name('admin.login');
+    Route::post('login', 'LoginController@login');
+    Route::post('logout', 'LoginController@logout')->name('admin.logout');
+    Route::get('register', 'RegisterController@showRegistrationForm')->name('admin.register');
+    Route::post('register', 'RegisterController@register');
+});
+
+Route::prefix('admin')->namespace('Admin')->group(function () {
+    Route::get('dashboard', 'DashboardController@index')->name('admin.dashboard');
+});
+//Admin routes ends here
+
